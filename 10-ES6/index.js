ed8274680e7adc19971d5709c2c8950123187b29
Aysan Isayo <aisayo@icloud.com>
2022-03-09 13:06:07 -0800

    solution

--- 10-ES6/index.js
+++ 10-ES6/index.js
@@ -1,110 +1,160 @@
+const BASE_URL = "http://localhost:3000";
+
 const pokeContainer = document.querySelector("#poke-container");
 const pokeForm = document.querySelector("#poke-form");
 const pokeFormContainer = document.querySelector("#poke-form-container");
 
-pokeForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const name = document.querySelector("#name-input").value;
-  const img = document.querySelector("#img-input").value;
+const createConfigObj = (method, body) => {
 
-  let newChar = {
-    name: name,
-    img: img,
-    likes: 0,
-  };
-
-  fetch("http://localhost:3000/characters", {
-    method: "POST",
+  const configObj = {
+    method,
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    body: JSON.stringify(newChar),
-  });
-  renderPokemon(newChar);
-  pokeForm.reset();
-});
+   body: JSON.stringify(body)
+  }
 
-function getPokemon() {
-  fetch("http://localhost:3000/characters")
-    .then(function (resp) {
-      if (resp.ok) {
-        return resp.json();
-      } else {
-        throw new Error(`${resp.status}: ${resp.statusText}`);
-      }
-    })
-    .then(function (resp) {
-      resp.forEach(function (char) {
-        renderPokemon(char);
-      });
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+  return configObj
 }
 
-getPokemon();
+const createPokemon = async (event) => {
+  event.preventDefault();
+  const name = document.querySelector("#name-input").value;
+  const img = document.querySelector("#img-input").value;
+
+  // ES6: simple object construction
+  // const newChar = {
+  //   name: name,
+  //   img: img,
+  //   likes: 0,
+  // };
+
+  // updated to:
+  const newChar = {
+    name, // when this gets compiled, compiler will read it: name: name
+    img,
+    likes: 0,
+  };
+
+  // pessimistic rendering:
+  let resp = await fetch(`${BASE_URL}/characters`, createConfigObj("POST", newChar));
+  let character = await resp.json(); // this will wait to complete before line 37 executes
+  renderPokemon(character);
+
+  // optimistically: no need for async/await
+  // we just simply make the fetch request, and render the character using the newChar objt
+  // fetch("http://localhost:3000/characters", configObj);
+  // renderPokemon(newChar)
+  pokeForm.reset();
+};
+
+const getPokemon = async () => {
+  try {
+    let resp = await fetch(`${BASE_URL}/characters`);
+    if (resp.ok) {
+      let characters = await resp.json();
+      characters.forEach(renderPokemon);
+    } else {
+      throw new Error(`${resp.status}: ${resp.statusText}`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  // fetch("http://localhost:3000/characters")
+  // .then(function (resp) {
+  //   if (resp.ok) {
+  //     return resp.json();
+  //   } else {
+  //     throw new Error(`${resp.status}: ${resp.statusText}`);
+  //   }
+  // })
+  // .then(function (resp) {
+  //   resp.forEach(function (char) {
+  //     renderPokemon(char);
+  //   });
+  // })
+  // .catch(function (err) {
+  //   console.log(err);
+  // });
+};
+
+const init = () => {
+  // pokeForm.addEventListener("submit", (event) => createPokemon(event));
+  pokeForm.addEventListener("submit", createPokemon);
+  getPokemon();
+};
+
+init();
+
+const createImgEl = (src, alt) => {
+  let img = document.createElement("img");
+  img.src = src;
+  img.alt = `${alt} image`;
+  return img;
+};
+
+// ES6 feature: default arguments: className = "pokedex"
+const createH3El = (textContent, className) => {
+  let h3 = document.createElement("h3");
+  h3.textContent = textContent;
+
+  // if (className){
+  //   h3.className = className
+  // }
+
+  // ternary operators
+  className ? (h3.className = className) : null;
+
+  return h3;
+};
+
+const createBttn = (textContent, className, eventHandler) => {
+  const bttn = document.createElement("button");
+  bttn.className = className;
+  bttn.textContent = textContent;
+  bttn.addEventListener("click", eventHandler);
+  return bttn;
+};
+
+const handleLike = async (e, pokemon, likeNum) => {
+  e.stopPropagation();
+  ++pokemon.likes;
+
+  let resp = await fetch(`${BASE_URL}/characters/${pokemon.id}`, createConfigObj("PATCH", { likes: pokemon.likes }));
+  let character = await resp.json();
+  likeNum.textContent = character.likes;
+};
+
+const handleDelete =  async (e, pokemon, pokeCard) => {
+  e.stopPropagation();
+
+  let resp = await fetch(`http://localhost:3000/characters/${pokemon.id}`, createConfigObj("DELETE", {}));
+  resp.ok ? pokeCard.remove() : null;
+};
+
+const createDiv = (id, className) => {
+  let div = document.createElement("div");
+  div.id = id;
+  className ? (div.className = className) : null;
+  return div;
+};
 
 function renderPokemon(pokemon) {
-  const pokeCard = document.createElement("div");
-  pokeCard.id = `poke-${pokemon.id}`;
-  pokeCard.className = "poke-card";
-
+  const pokeCard = createDiv(`poke-${pokemon.id}`, "poke-card");
   pokeCard.addEventListener("click", function () {
     return showCharacter(pokemon);
   });
-
-  const pokeImg = document.createElement("img");
-  pokeImg.src = pokemon.img;
-  pokeImg.alt = `${pokemon.name} image`;
-
-  const pokeName = document.createElement("h3");
-  pokeName.textContent = pokemon.name;
-
-  const pokeLikes = document.createElement("h3");
-  pokeLikes.textContent = "Likes: ";
-
-  const likeNum = document.createElement("h3");
-  likeNum.className = "likes-num";
-  likeNum.textContent = pokemon.likes;
-
-  const likesBttn = document.createElement("button");
-  likesBttn.className = "like-bttn";
-  likesBttn.textContent = "♥";
-  likesBttn.addEventListener("click", function (e) {
-    e.stopPropagation();
-    ++pokemon.likes;
-
-    fetch(`http://localhost:3000/characters/${pokemon.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({ likes: pokemon.likes }),
-    });
-
-    likeNum.textContent = pokemon.likes;
-  });
-
-  const deleteBttn = document.createElement("button");
-  deleteBttn.className = "delete-bttn";
-  deleteBttn.textContent = "delete";
-  deleteBttn.addEventListener("click", function (e) {
-    e.stopPropagation();
-    fetch(`http://localhost:3000/characters/${pokemon.id}`, {
-      method: "DELETE",
-    })
-      .then(function (resp) {
-        console.log(resp);
-        return resp.json();
-      })
-      .then(function () {
-        pokeCard.remove();
-      });
-
-  });
+  const pokeImg = createImgEl(pokemon.img, pokemon.name);
+  const pokeName = createH3El(pokemon.name);
+  const pokeLikes = createH3El("Likes: ");
+  const likeNum = createH3El(pokemon.likes, "likes-num");
+  const likesBttn = createBttn("♥", "like-bttn", (e) =>
+    handleLike(e, pokemon, likeNum)
+  );
+  const deleteBttn = createBttn("delete", "delete-bttn", (e) =>
+    handleDelete(e, pokemon, pokeCard)
+  );
 
   pokeCard.append(pokeImg, pokeName, pokeLikes, likeNum, likesBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
@@ -193,8 +243,7 @@ function loadComments(pokeCard, char) {
   const commentsDiv = document.createElement("div");
   commentsDiv.id = `comment-card-${char.id}`;
 
-  const h3 = document.createElement("h3");
-  h3.textContent = `${char.comments.length} comments:`;
+  const h3 = createH3El(`${char.comments.length} comments:`);
 
   commentsDiv.append(h3);
   pokeCard.append(commentsDiv);
