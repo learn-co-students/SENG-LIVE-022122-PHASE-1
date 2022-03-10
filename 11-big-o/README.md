687b38dc898e1d0fc4d0e11fa8d952fe61fa7470
Aysan Isayo <aisayo@icloud.com>
2022-03-09 18:36:39 -0800

    starter code

---
+++ 11-big-o/README.md
@@ -0,0 +1,15 @@
+# Big O & Asymptotic Notation
+
+### Learning Goals
+
+## Objectives
+
+- Understand asymptotic notation and its varied applications for calculating program runtime
+
+## Deliverables
+
+- Discuss:
+  - Constant Time Complexity
+  - Logarithmic Time Complexity
+  - Linear Time Complexity
+  - Quadratic Time Complexity
---
+++ 11-big-o/index.js
@@ -0,0 +1,86 @@
+// Constant Time Complexity: Big O(1)
+
+function multipleByTwo(n) {
+  return 2 * n;
+}
+
+// multipleByTwo(10)
+// multipleByTwo(3000)
+
+// Linear Time Complexity: Big O(N)
+
+function reverseArr(originalArray) {
+  let newArray = [];
+  for (let i = originalArray.length - 1; i >= 0; i--) {
+    newArray.push(originalArray[i]);
+  }
+
+  return newArray;
+}
+
+// reverseArr([4, 5, 6])
+// reverseArr([6, 5, 4, 3, 2, 1])
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// Optimize the previous code to O(1)
+
+// function addUpTo(n) {
+//   return n * (n+1) / 2;
+// }
+
+// Logarithmic Time Complexity: Big O(log n)
+
+const binarySearch = (arr, item) => {
+  let start = 0;
+  // set end to the index of the last element in the array
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    // finding the middle of the array
+    const middle = Math.floor((start + end) / 2);
+    // if the element in the middle matches the
+    // item we are searching for, return and exit
+    if (arr[middle] === item) {
+      return middle;
+    }
+
+    // if the element in the middle is greater than the item
+    // look to the left of the data set
+    if (arr[middle] > item) {
+      end = middle - 1;
+      // else look to the right of the data set
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return null; //if not found
+};
+
+// binarySearch([1, 2, 3, 4, 5], 10); // null
+// binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 7); // 6
+
+// Quadratic Time Complexity: Big O(N^2)
+
+function multAllElements(arr1, arr2) {
+  if (arr1.length !== arr2.length) return undefined;
+
+  let sumOfProducts = 0;
+
+  for (let el of arr1) {
+    for (let subEl of arr2) {
+      sumOfProducts += el * subEl;
+    }
+  }
+  return sumOfProducts;
+}
+
+// multAllElements([1, 2], [9, 10])
+// multAllElements([1, 2, 3, 4], [5, 6, 7, 8])
